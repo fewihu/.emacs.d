b2f62ab61a25db38f014cc5d4fab3aa63fb8e1cf
Felix Müller <felix.mueller@cloudandheat.com>
2024-01-15 10:39:45 +0100

    Let eww open urls

--- init.el
+++ init.el
@@ -277,6 +277,20 @@ conventions are checked."
 (require 'yasnippet)
 (yas-global-mode 1)
 
+;; ----------
+;; let eww open links
+
+(defun my-set-eww-buffer-title ()
+  (let* ((title  (plist-get eww-data :title))
+         (url    (plist-get eww-data :url))
+         (result (concat "*eww-" (or title
+                              (if (string-match "://" url)
+                                  (substring url (match-beginning 0))
+                                url)) "*")))
+    (rename-buffer result t)))
+
+(add-hook 'eww-after-render-hook 'my-set-eww-buffer-title)
+
 ;; ================================
 ;; Org-Mode the initial reason to use Emacs
 
