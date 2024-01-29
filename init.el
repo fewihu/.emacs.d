65b8c4de06b06eed863556ef595d912873f576a6
fewihu <felix.mueller.97@proton.me>
2024-01-29 09:38:11 +0100

    Add keybindings to resize window

--- init.el
+++ init.el
@@ -64,6 +64,13 @@
 ;; ================================
 ;; feel
 
+;; sensefull key-bindings to size windows
+;; https://www.emacswiki.org/emacs/WindowResize
+(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "S-C-<down>") 'shrink-window)
+(global-set-key (kbd "S-C-<up>") 'enlarge-window)
+
 ;; change "focus" to new split windows
 (global-set-key "\C-x2" (lambda ()
 			  (interactive)(split-window-vertically) (other-window 1)))
